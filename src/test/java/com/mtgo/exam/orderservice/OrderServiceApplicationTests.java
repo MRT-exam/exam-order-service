ca3c797c44d5c9ab6308f06fe8085ab31963be27
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-12-05 15:31:33 +0100

    add component scan to OrderServiceApplicationTest

--- src/test/java/com/mtgo/exam/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/mtgo/exam/orderservice/OrderServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.mtgo.exam.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan(basePackages = "com.mtgo.exam.orderservice")
 class OrderServiceApplicationTests {
 	@Test
 	void contextLoads() {
