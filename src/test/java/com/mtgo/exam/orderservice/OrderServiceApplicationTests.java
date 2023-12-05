1d890c582f0efdc9dee1ec31910bebe3d34d579c
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-12-05 16:48:26 +0100

    small change

--- src/test/java/com/mtgo/exam/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/mtgo/exam/orderservice/OrderServiceApplicationTests.java
@@ -2,9 +2,10 @@ package com.mtgo.exam.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest(classes = OrderServiceApplication.class)
-
+@ComponentScan(basePackages = "com.mtgo.exam.orderservice")
 class OrderServiceApplicationTests {
 
 }
